78fa1fa753be0094fb52916f80a8be08273b6e17
Kristin0 <44585557+Kristin0@users.noreply.github.com>
2021-05-13 14:43:41 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,19 +28,19 @@ terraform {
 }
 
 module "vpc" {
-  source = "github.com/Kristin0/terraform-modules/aws_network"
+  source = "github.com/Kristin0/terraform-modules/tree/main/aws_network"
   instance_id = module.server.ec2_instance_id
 }
 
 module "database" {
-  source = "github.com/Kristin0/terraform-modules/aws_database"
+  source = "github.com/Kristin0/terraform-modules/tree/main/aws_database"
   subnet_ids = module.vpc.subnet_private_ids
   security_group_id = module.vpc.http_security_group_id
 
 } 
 
 module "server" {
-  source = "github.com/Kristin0/terraform-modules/aws_server"
+  source = "github.com/Kristin0/terraform-modules/tree/main/aws_server"
   security_group_id = module.vpc.http_security_group_id
   public_ip_ranges = module.vpc.public_ip_ranges
   subnet_public_ids = module.vpc.subnet_public_ids
